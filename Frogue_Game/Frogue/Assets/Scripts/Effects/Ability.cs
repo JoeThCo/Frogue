64debcec0243a1fb81d8af25bbe00af86370e52d
Joe Colley <josephthco@gmail.com>
2024-06-29 12:45:43 -0400

    icons, so, resource manger

--- Frogue_Game/Frogue/Assets/Scripts/Effects/Ability.cs
+++ Frogue_Game/Frogue/Assets/Scripts/Effects/Ability.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Ability", menuName = "ScriptableObjects/Ability")]
-public class Ability : ScriptableObject
+//[CreateAssetMenu(fileName = "Ability", menuName = "ScriptableObjects/Ability")]
+public class Ability
 {
     [SerializeField] private Trigger trigger;
     [SerializeField] private Effect effect;
     [SerializeField] private EffectWho effectWho;
+
+    public Ability() 
+    {
+        
+    }
 }
