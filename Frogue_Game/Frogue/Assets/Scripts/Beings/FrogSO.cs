71649ef23b8d05b69c0dbea93e3eb853aac87ba9
Joe Colley <josephthco@gmail.com>
2024-11-11 16:13:42 -0500

    script reset :(

--- Frogue_Game/Frogue/Assets/Scripts/Beings/FrogSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Frog", menuName = "ScriptableObject/Frog")]
-public class FrogSO : BeingSO
-{
-    //abilities
-}
